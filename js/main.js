fc5d8a9b81ae241edd481d014569e873feddd5c0
haruki yamaguchi <hrk1106hasu@gmail.com>
2022-09-19 22:31:18 +0900

    finish

--- js/main.js
+++ js/main.js
@@ -1,115 +1,140 @@
 'use strict';
 
+console.clear();
+
 {
-  let year=2020;
-  let month=8;
+  const today = new Date();
+  let year = today.getFullYear();
+  let month = today.getMonth();
 
-  // 前月の日付を取得し無効化
+  function getCalendarHead() {
+    const dates = [];
+    const d = new Date(year, month, 0).getDate();
+    const n = new Date(year, month, 1).getDay();
 
-  const getCalendarHead=()=>{
-    const dates=[];
-    const d=new Date(year,month,0).getDate();
-    const n=new Date(year,month,1).getDay();
-
-    for(let i=0; i<n; i++){
+    for (let i = 0; i < n; i++) {
+      // 30
+      // 29, 30
+      // 28, 29, 30
       dates.unshift({
-        date: d-i,
+        date: d - i,
         isToday: false,
         isDisabled: true,
       });
     }
+
     return dates;
   }
 
-  // 当月の日付を取得
+  function getCalendarBody() {
+    const dates = []; // date: 日付, day: 曜日
+    const lastDate = new Date(year, month + 1, 0).getDate();
 
-  const getCalendarBody=()=>{
-    const dates=[];
-    const lastDate=new Date(year,month+1,0).getDate();
-
-    for(let i=1; i<=lastDate; i++){
+    for (let i = 1; i <= lastDate; i++) {
       dates.push({
         date: i,
         isToday: false,
         isDisabled: false,
       });
     }
+
+    if (year === today.getFullYear() && month === today.getMonth()) {
+      dates[today.getDate() - 1].isToday = true;
+    }
+
     return dates;
   }
 
-  // 来月の日付を取得し無効化
+  function getCalendarTail() {
+    const dates = [];
+    const lastDay = new Date(year, month + 1, 0).getDay();
 
-  const getCalendarTail=()=>{
-    const dates=[];
-    const lastDay=new Date(year,month+1,0).getDay();
-    
-    for(let i=1; i<7-lastDay; i++){
+    for (let i = 1; i < 7 - lastDay; i++) {
       dates.push({
         date: i,
         isToday: false,
         isDisabled: true,
       });
     }
+
     return dates;
   }
 
-  const createCalendar=()=>{
-    const tbody=document.querySelector('tbody');
+  function clearCalendar() {
+    const tbody = document.querySelector('tbody');
 
-    while(tbody.firstChild) {
+    while (tbody.firstChild) {
       tbody.removeChild(tbody.firstChild);
     }
+  }
 
-    const title=`${year}/${String(month+1).padStart(2,'0')}`;
-    document.getElementById('title').textContent=title;
+  function renderTitle() {
+    const title = `${year}/${String(month + 1).padStart(2, '0')}`;
+    document.getElementById('title').textContent = title;
+  }
 
-    const dates=[
+  function renderWeeks() {
+    const dates = [
       ...getCalendarHead(),
       ...getCalendarBody(),
       ...getCalendarTail(),
     ];
+    const weeks = [];
+    const weeksCount = dates.length / 7;
 
-    const weeks=[];
-    const weeksCount=dates.length/7;
-
-    for(let i=0; i<weeksCount; i++){
-      weeks.push(dates.splice(0,7));
+    for (let i = 0; i < weeksCount; i++) {
+      weeks.push(dates.splice(0, 7));
     }
 
-    weeks.forEach((week)=>{
-      const tr=document.createElement('tr');
+    weeks.forEach(week => {
+      const tr = document.createElement('tr');
+      week.forEach(date => {
+        const td = document.createElement('td');
 
-      week.forEach((date)=>{
-        const td=document.createElement('td');
-        td.textContent=date.date;
-
-        if(date.isToday){
+        td.textContent = date.date;
+        if (date.isToday) {
           td.classList.add('today');
         }
-        if(date.isDisabled){
+        if (date.isDisabled) {
           td.classList.add('disabled');
         }
+
         tr.appendChild(td);
       });
       document.querySelector('tbody').appendChild(tr);
     });
   }
 
-  document.getElementById('prev').addEventListener('click',()=>{
+  function createCalendar() {
+    clearCalendar();
+    renderTitle();
+    renderWeeks();
+  }
+
+  document.getElementById('prev').addEventListener('click', () => {
     month--;
-    if(month<0){
+    if (month < 0) {
       year--;
-      month=11;
+      month = 11;
     }
+
     createCalendar();
   });
 
-  document.getElementById('next').addEventListener('click',()=>{
+  document.getElementById('next').addEventListener('click', () => {
     month++;
-    if(month>11){
+    if (month > 11) {
       year++;
-      month=0;
+      month = 0;
     }
+
+    createCalendar();
+  });
+
+  document.getElementById('today').addEventListener('click', () => {
+    year = today.getFullYear();
+    month = today.getMonth();
+
     createCalendar();
   });
 
